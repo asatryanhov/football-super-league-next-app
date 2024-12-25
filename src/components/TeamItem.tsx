354943e018d1721602179ce61892f78c5e848c8a
Hov <asatryanhovhannes1995@gmail.com>
2024-12-25 11:14:27 +0100

    added logo type

--- src/components/TeamItem.tsx
+++ src/components/TeamItem.tsx
@@ -15,6 +15,7 @@ interface TeamItemProps {
     avgRating?: number;
     goalsScored?: number;
     assists?: number;
+    logo?: string; // Добавьте эту строку
     [key: string]: any;
   };
   teamName: string;
