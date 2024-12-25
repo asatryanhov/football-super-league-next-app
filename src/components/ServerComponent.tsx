6f9084b0d56dddfee821168efb91638c442330db
Hov <asatryanhovhannes1995@gmail.com>
2024-12-25 14:27:03 +0100

    added ServerComponentProps

--- src/components/ServerComponent.tsx
+++ src/components/ServerComponent.tsx
@@ -1,12 +1,19 @@
 import { fetchData } from "@/lib/fetchData";
 import ClientComponent from "@/components/ClientComponent";
 
+interface ServerComponentProps {
+  teamName: string;
+  teamId: string;
+  tournamentId: string;
+  seasonId: string;
+}
+
 export default async function ServerComponent({
   teamName,
   teamId,
   tournamentId,
   seasonId,
-}) {
+}: ServerComponentProps) {
   const data = await fetchData(teamId, tournamentId, seasonId);
 
   return <ClientComponent initialData={data} teamName={teamName} />;
