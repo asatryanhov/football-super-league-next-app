8f4a812b49ef315172be8a125968d49ae33bd5d0
Hov <asatryanhovhannes1995@gmail.com>
2025-09-15 02:56:55 +0200

    vercel update every monday 00:59

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 // import App from "@/components/App";
 import App from "@/components/App";
 import League from "@/components/League";
+
+// Принудительно делаем страницу динамической чтобы избежать API лимитов во время build
+export const dynamic = "force-dynamic";
+
 //update
 export default function Home() {
   return (
